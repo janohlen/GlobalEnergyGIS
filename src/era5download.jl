4302cb1d57c0eddce60e696a3b9d50563b1cca20
Jan Ohlenbusch <73192940+janohlen@users.noreply.github.com>
2021-10-11 20:12:12 +0200

    Update era5download.jl

--- src/era5download.jl
+++ src/era5download.jl
@@ -35,7 +35,7 @@ function cds_id(uid::Int, api_key::AbstractString)
     end
 end
 
-function download_and_convert_era5(year=2018; datasets=["wind", "solar", "temp"])
+function download_and_convert_era5(year=2018; month = 1; datasets=["wind", "solar", "temp"])
     for dataset in datasets
         println("\nDownloading ERA5 $dataset data from Copernicus...")
         era5download(year; datasets=[dataset])
